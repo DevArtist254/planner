aaf626a86eba91046cb1d410cd59b4c816a2a20b
Kevin wasonga <kevinwasonga415@outlook.com>
2025-04-02 05:48:46 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+body {
+  background: #f2f2f2;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
 }
 
-nav {
+/* nav {
   padding: 30px;
 }
 
@@ -26,5 +30,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
---
+++ src/components/SingleProject.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="project">
+        <div class="actions" @click="toggleDetails">
+            <h3>{{ project.title }}</h3>
+        </div>
+        <div class="details" v-show="tD">
+            <p>{{ project.details }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SingleProject",
+    props: ['project'],
+    data() {
+        return {
+            tD: false
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.tD = !this.tD
+        }
+    }
+}
+</script>
+
+<style>
+.project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+    border-left: 4px solid #e90074;
+}
+
+h3 {
+    cursor: pointer;
+}
+</style>
